6c0067ce4a16bde9abbf5d4fee6fb30a32f8e570
eestevekpmp <94544051+eestevekpmp@users.noreply.github.com>
2023-04-02 21:04:46 -0400

    Update app.py

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,74 @@
 # Import the dependencies.
 
-
+import numpy as np
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify 
 
 #################################################
 # Database Setup
 #################################################
-
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with = engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(bind=engine)
 
 #################################################
 # Flask Setup
 #################################################
-
+app = Flask(__name__)
 
 
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route("/") #establishes the home route for the server
+def home():
+    
+    print("List all the available routes.")
+    return (
+        f"Available routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0[start_date format:yyyy-mm-dd]<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    session = Session(engine)
+
+    prcp_scores = session.query(measurement.date, measurement.prcp).\
+        filter(measurement.date >= "2016-08-24").all()
+    session.close()
+# Convert prcp_scores query results to a dictionary.
+prcp_data = []
+for date,prcp in prcp_scores:
+    prcp_dict = {}
+    prcp_dict["date"] = date
+    prcp_dict["prcp"] = prcp
+
+    prcp_data.append(prcp_dict)
+
+return jsonify(prcp_data)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
